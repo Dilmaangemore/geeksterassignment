3c4737826fd98b1958d7dc7336d30e7c9d729df6
Dilmaangemore <90092966+Dilmaangemore@users.noreply.github.com>
2022-01-06 21:13:39 +0530

    quicksort

--- quickandmerge/quicksort.java
+++ quickandmerge/quicksort.java
@@ -16,20 +16,14 @@ public class quicksort {
 	    arr[j] = temp;
 	}
 
-	/* This function takes last element as pivot, places
-	   the pivot element at its correct position in sorted
-	   array, and places all smaller (smaller than pivot)
-	   to left of pivot and all greater elements to right
-	   of pivot */
+	
 	static int partition(int[] arr, int low, int high)
 	{
 	    
 	    // pivot
 	    int pivot = arr[high]; 
 	    
-	    // Index of smaller element and
-	    // indicates the right position
-	    // of pivot found so far
+	   
 	    int i = (low - 1); 
 
 	    for(int j = low; j <= high - 1; j++)
